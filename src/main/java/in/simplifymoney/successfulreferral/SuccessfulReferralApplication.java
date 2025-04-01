82dba679ac59805e1c53118b147f93e4516fb8db
antariksha-sharma <helloantariksha@gmail.com>
2025-04-01 22:43:06 +0530

    Initial commit with added dependencies

---
+++ src/main/java/in/simplifymoney/successfulreferral/SuccessfulReferralApplication.java
@@ -0,0 +1,13 @@
+package in.simplifymoney.successfulreferral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessfulReferralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccessfulReferralApplication.class, args);
+	}
+
+}
